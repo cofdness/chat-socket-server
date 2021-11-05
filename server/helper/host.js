43402b96e6ae3b697a944c344a2e69f56e526ec4
Vu Tran Kim Phung <vtkp2002dn@gmail.com>
2021-11-05 20:09:33 +0700

    add some log for debug heroku

--- server/helper/host.js
+++ server/helper/host.js
@@ -1,4 +1,4 @@
 import { client_ip, client_port, client_protocol, ip, port, protocol } from '../config'
 
-export const server_uri = `${protocol}://${ip}:${port}`
+export const server_uri = `${protocol}://${ip}`
 export const client_uri = `${client_protocol}://${client_ip}:${client_port}`
